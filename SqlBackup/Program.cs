2dbf382613c49c97fd332b12b2c3857ba28918b3
Nishihara <nishihara@gmail.com>
2012-09-06 15:47:23 -0300

    First commit

---
+++ SqlBackup/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.SqlServer.Management.Smo;
+using Microsoft.SqlServer.Management.Common;
+
+namespace SqlBackup
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 3) return;
+            string serverName = args[0];
+            string database = args[1];
+            string filePath = args[2];
+
+            Server srv = new Server(@serverName);
+            Backup backup = new Backup();
+
+            // specify what you want to backup
+            backup.Action = BackupActionType.Database;
+
+            // specify the name of the database
+            backup.Database = database;
+            
+            // specify what kind of devides to use, in this example we are using the File Device
+            backup.Devices.AddDevice(@filePath, DeviceType.File);
+            backup.BackupSetName = "Example database backup";
+            backup.BackupSetDescription = "Example database full backup";
+
+            // setting the expiration date
+            backup.ExpirationDate = DateTime.Today.AddDays(10);
+
+            // setting incremental backup
+            backup.Incremental = true;
+
+            // events called to show progress and when the backup finish
+            backup.PercentComplete += CompletionStatusInPercent;
+            backup.Complete += Backup_Completed;
+
+            // start backup, this method is asynchronous
+            backup.SqlBackup(srv);
+        }
+
+        private static void CompletionStatusInPercent(object sender, PercentCompleteEventArgs args)
+        {
+            Console.Clear();
+            Console.WriteLine("Percent completed: {0}%.", args.Percent);
+        }
+        private static void Backup_Completed(object sender, ServerMessageEventArgs args)
+        {
+            Console.WriteLine("Backup completed.");
+            Console.WriteLine(args.Error.Message);
+        }
+    }
+}
---
+++ SqlBackup/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("SqlBackup")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("SqlBackup")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2012")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("1af33370-259e-4a58-b83b-10281ec434b8")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
